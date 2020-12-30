452d406b5d1c1377c07e14fd53606a37cab2f498
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-30 00:16:40 -0300

    Separate samples

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,58 +1,10 @@
 ﻿using System;
-using System.Linq;
-using Simple.Sqlite;
 using Simple.Sqlite.Attributes;
 
-SqliteDB db = new SqliteDB("myStuff.db");
-Console.WriteLine($"Database is at {db.DatabaseFileName}");
+// Choose your example
+Test.Sample.FullCycle.run();
+Test.Sample.DocumentStorage.run();
 
-// Create a DB Schema
-db.CreateTables()
-  .Add<MyData>()
-  .Commit();
-
-var d = new MyData()
-{
-    MyId = (int)DateTimeOffset.Now.ToUnixTimeSeconds(),
-    MyName = "My name is bob",
-    MyBirthDate = DateTime.Now,
-    MyUID = Guid.NewGuid(),
-    MyWebsite = new Uri("http://example.com"),
-    MyDecimalValue = 123.4M,
-    MyDoubleValue = 456.7,
-    MyFloatValue = 789.3f,
-    MyEnum = MyData.eIntEnum.Zero,
-};
-Console.WriteLine($"New data inserted: Id={d.MyId} MyUID={d.MyUID}");
-db.Insert(d);
-
-// get all data
-var allData = db.GetAll<MyData>();
-
-Console.WriteLine("All data:");
-foreach (var rowData in allData)
-{
-    Console.WriteLine($" > {rowData.MyId} {rowData.MyName} {rowData.MyUID}");
-}
-
-//get "bob" data
-var bobs = db.ExecuteQuery<MyData>("SELECT * FROM MyData WHERE MyName LIKE @name ", new { name = "%bob%" });
-Console.WriteLine("All bob data:");
-foreach (var rowData in bobs)
-{
-    Console.WriteLine($" > {rowData.MyId} {rowData.MyName} {rowData.MyUID}");
-}
-
-// change frst bob
-var firstBob = bobs.First();
-firstBob.MyName = "Changed bob";
-db.InsertOrReplace(firstBob);
-// show all data again
-Console.WriteLine("All data:");
-foreach (var rowData in allData)
-{
-    Console.WriteLine($" > {rowData.MyId} {rowData.MyName} {rowData.MyUID}");
-}
 
 
 public class MyData
---
+++ Test/Sample/DocumentStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple.Sqlite;
+
+namespace Test.Sample
+{
+    public class DocumentStorage
+    {
+        public static void run()
+        {
+            NoSqliteDoc db = new NoSqliteDoc("myStuff.db");
+            Console.WriteLine($"Database is at {db.DatabaseFileName}");
+
+
+
+        }
+    }
+}
