d858cf57e2d2dd32f25febbd9c944a6029915971
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-31 20:32:45 -0300

    Add first test

---
+++ UnitTests/Helpers/TestDBBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Simple.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Helpers
+{
+    public static class TestDBBuilder
+    {
+        public static readonly string IN_MEMORY = "FullUri=file:instance.db?mode=memory&cache=shared";
+
+        public static SqliteDB Create() => new SqliteDB(":memory:");
+    }
+}
---
+++ UnitTests/SqliteDBTests/BasicTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.SqliteDBTests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void SqliteDB_TestInMemoryDBOpen()
+        {
+            var db = Helpers.TestDBBuilder.Create();
+            Assert.Equal(10, db.ExecuteScalar<int>("SELECT 10", null));
+        }
+    }
+}
