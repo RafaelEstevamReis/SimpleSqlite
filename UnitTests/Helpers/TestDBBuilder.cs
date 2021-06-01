211a6bff4bd9a5a095264d62b7ed472f5432e934
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-31 23:07:10 -0300

    Update TestDBBuilder.cs

--- UnitTests/Helpers/TestDBBuilder.cs
+++ UnitTests/Helpers/TestDBBuilder.cs
@@ -4,8 +4,8 @@ namespace UnitTests.Helpers
 {
     public static class TestDBBuilder
     {
-        public static readonly string IN_MEMORY = "FullUri=file:instance.db?mode=memory&cache=shared";
+        public static readonly string IN_MEMORY = ":memory:";
 
-        public static SqliteDB Create() => new SqliteDB(":memory:");
+        public static SqliteDB Create() => new SqliteDB(IN_MEMORY);
     }
 }
