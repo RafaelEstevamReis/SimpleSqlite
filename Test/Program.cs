8a6903628815ddde6a87c0a0aa42ae7119db724e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-06-03 14:22:02 -0300

    Fix Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Simple.DatabaseWrapper.Attributes;
 
 // Choose your example
 Console.WriteLine("Choose one example");
-Test.Sample.FullCycle.run();
+//Test.Sample.FullCycle.run();
 //Test.Sample.IntPrimaryKeyExample.run();
 //Test.Sample.SimpleTypeQuery.run();
 //Test.Sample.DocumentStorage.run();
