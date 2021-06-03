d837d868d453adf4b4a75f568bf3c9de49719db7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-06-03 15:37:55 -0300

    First test

---
+++ UnitTest/SqliteDBTests/SqliteBaseTests.cs
@@ -0,0 +1,16 @@
+﻿using Simple.Sqlite;
+using Xunit;
+
+namespace UnitTest.SqliteDBTests
+{
+    public class SqliteBaseTests
+    {
+        [Fact]
+        public void SqliteDB_TestConnection()
+        {
+            var db = SqliteDB.CreateInMemory();
+            Assert.Equal(1, db.ExecuteScalar<int>("SELECT 1", null));
+        }
+
+    }
+}
