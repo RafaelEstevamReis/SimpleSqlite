b47c88860f498fed77196e4246a8ea2271a9d984
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-06-03 18:58:14 -0300

    Add more tests

---
+++ UnitTest/SqliteDBTests/SqliteGetAllTables.cs
@@ -0,0 +1,36 @@
+﻿using Simple.Sqlite;
+using Xunit;
+
+namespace UnitTest.SqliteDBTests
+{
+    public class SqliteGetAllTables
+    {
+        [Fact]
+        public void SqliteDB_GetAllTables_Empty()
+        {
+            var db = SqliteDB.CreateInMemory();
+            
+            // Asser that there are no tables
+            int count = db.ExecuteScalar<int>("SELECT COUNT(1) FROM sqlite_master WHERE type='table'", null);
+            Assert.Equal(0, count);
+            // Check that matches with GetAll
+            Assert.Empty(db.GetAllTables());
+        }
+
+        [Fact]
+        public void SqliteDB_GetAllTables_Create()
+        {
+            var db = SqliteDB.CreateInMemory();
+            Assert.Empty(db.GetAllTables());
+
+            // Creates a new table and check it
+            db.Execute("CREATE TABLE test (c1, c2)", null);
+
+            // Should be one table
+            var tables = db.GetAllTables();
+            Assert.Single(tables);
+
+            Assert.Equal("test", tables[0]);
+        }
+    }
+}
