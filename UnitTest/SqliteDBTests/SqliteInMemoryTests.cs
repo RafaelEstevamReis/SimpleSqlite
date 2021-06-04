8ab209f60ff8f4cb67efdc6b52570a2d09c284f8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-06-03 22:51:08 -0300

    Fix test

--- UnitTest/SqliteDBTests/SqliteInMemoryTests.cs
+++ UnitTest/SqliteDBTests/SqliteInMemoryTests.cs
@@ -9,14 +9,8 @@ namespace UnitTest.SqliteDBTests
         public void SqliteDB_InMemory_FileName()
         {
             var db = SqliteDB.CreateInMemory();
-            // Each new instance creates a new number to allow shared access to unique databases
-            // For that, each new test has a new number
-            // the test, cannot know the current number
-            // "InMemoryXXX"
-            Assert.StartsWith("InMemory", db.DatabaseFileName);
-
+            Assert.StartsWith("", db.DatabaseFileName);
             Assert.True(db.IsInMemoryDatabase);
-
         }
     }
 }
