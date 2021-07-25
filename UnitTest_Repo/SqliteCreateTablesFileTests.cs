ad3f32bd37981d5f53fa9dd270a1e06aa79367b5
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-07-25 12:39:04 -0300

    Add repo testing

---
+++ UnitTest_Repo/SqliteCreateTablesFileTests.cs
@@ -0,0 +1,28 @@
+﻿using UnitTest_Repo.TestModels;
+using Xunit;
+
+namespace UnitTest_Repo.SqliteDBTests
+{
+    public class SqliteCreateTablesFileTests : SqliteFileTests
+    {
+        [Fact]
+        public void SqliteDB_File_CreateTable_Result()
+        {
+            var db = GetDb();
+            // Assert that thar are no tables
+            Assert.Empty(db.GetAllTables());
+
+            // Create the table
+            var result = db.CreateTables()
+                .Add<SimpleModel>()
+                .Commit();
+
+            // One table was created
+            Assert.Single(result);
+            // The one is the model
+            Assert.Equal("SimpleModel", result[0].TableName);
+            Assert.True(result[0].WasTableCreated);
+            Assert.Empty(result[0].ColumnsAdded);
+        }
+    }
+}
---
+++ UnitTest_Repo/SqliteFileTests.cs
@@ -0,0 +1,31 @@
+﻿using Simple.Sqlite;
+using System;
+using System.IO;
+using Xunit;
+
+namespace UnitTest_Repo.SqliteDBTests
+{
+    public class SqliteFileTests : IDisposable
+    {
+        private readonly FileInfo file;
+
+        public SqliteFileTests()
+        {
+            file = new FileInfo($"demoDb_{Guid.NewGuid()}.db");
+        }
+        protected SqliteDB GetDb() => new SqliteDB(file.FullName);
+
+        [Fact]
+        public void SqliteDB_TestConnection()
+        {
+            var db = GetDb();
+            Assert.Equal(1, db.ExecuteScalar<int>("SELECT 1", null));
+        }
+
+        public void Dispose()
+        {
+            //if (file.Exists) file.Delete();
+            if (File.Exists(file.FullName)) file.Delete();
+        }
+    }
+}
