03546f166e6a0efddb952915c348a5d43a36881e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-11-06 23:59:57 -0300

    Add migration

--- Simple.Sqlite/Extension/MigrationExtension.cs
+++ Simple.Sqlite/Extension/MigrationExtension.cs
@@ -1,10 +1,9 @@
-﻿namespace Simple.Sqlite.Extension
+﻿using Simple.DatabaseWrapper.Interfaces;
+
+namespace Simple.Sqlite.Extension
 {
     public static class MigrationExtension
     {
-        //public static ITableMapper CreateTables(this ISqliteConnection Connection)
-        //{
-        //    return new TableMapper(this, typeCollection);
-        //}
+        public static ITableMapper CreateTables(this ISqliteConnection Connection) => new TableMapperNew(Connection);
     }
 }
