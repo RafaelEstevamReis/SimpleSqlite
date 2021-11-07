517ca5ee5f4b21427130d1118ea0b789618cd25f
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-11-07 00:12:30 -0300

    Add Insert

---
+++ Simple.Sqlite/Extension/InsertExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace Simple.Sqlite.Extension
+{
+    public static class InsertExtension
+    {
+        public static long Insert<T>(this ISqliteConnection connection, T item, OnConflict resolution = OnConflict.Abort, string tableName = null)
+              => connection.ExecuteScalar<long>(HelperFunctions.buildInsertSql<T>(connection.typeCollection, resolution, tableName), item);
+
+    }
+}
