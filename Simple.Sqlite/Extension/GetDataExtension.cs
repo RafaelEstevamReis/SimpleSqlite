54ee9003052277ade43ff10aa21e4c56548a9b54
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-11-07 00:53:46 -0300

    Add GetAll

---
+++ Simple.Sqlite/Extension/GetDataExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Simple.Sqlite.Extension
+{
+    public static class GetDataExtension
+    {
+        public static IEnumerable<T> GetAll<T>(this ISqliteConnection connection)
+            => connection.Query<T>($"SELECT * FROM {typeof(T).Name} ", null);
+
+    }
+}
