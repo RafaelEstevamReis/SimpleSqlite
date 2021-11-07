ccb7102fe4a22de88e14205478e3da7a2cc17b54
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-11-07 00:08:37 -0300

    Separete helper

--- Simple.Sqlite/HelperFunctions.cs
+++ Simple.Sqlite/HelperFunctions.cs
@@ -2,6 +2,7 @@
 using Simple.DatabaseWrapper.Helpers;
 using Simple.DatabaseWrapper.TypeReader;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
@@ -53,5 +54,40 @@ namespace Simple.Sqlite
                              .ToArray();
         }
 
+        internal static string buildInsertSql<T>(ReaderCachedCollection typeCollection, OnConflict resolution, string tableName = null)
+        {
+            var info = typeCollection.GetInfo<T>();
+            if (tableName == null) tableName = info.TypeName;
+
+            var names = getNames(info, !info.IsAnonymousType);
+            var fields = string.Join(",", names);
+            var values = string.Join(",", names.Select(n => $"@{n}"));
+
+            if (resolution == OnConflict.Abort)
+            {
+                return $"INSERT INTO {tableName} ({fields}) VALUES ({values}); SELECT last_insert_rowid();";
+            }
+            else
+            {
+                string txtConflict = resolution switch
+                {
+                    OnConflict.RollBack => "ROLLBACK",
+                    OnConflict.Fail => "FAIL",
+                    OnConflict.Ignore => "IGNORE",
+                    OnConflict.Replace => "REPLACE",
+                    _ => throw new ArgumentException($"Invalid resolution: {resolution}"),
+                };
+
+                return $"INSERT OR {txtConflict} INTO {tableName} ({fields}) VALUES ({values}); SELECT last_insert_rowid();";
+            }
+        }
+        private static IEnumerable<string> getNames(TypeInfo type, bool needWrite)
+        {
+            return type.Items
+                       .Where(o => !o.Is(DatabaseWrapper.ColumnAttributes.Ignore))
+                       .Where(o => o.CanRead)
+                       .Where(o => !needWrite || o.CanWrite) // Be careful with NOTs and ORs
+                       .Select(o => o.Name);
+        }
     }
 }
--- Simple.Sqlite/SqliteDB.cs
+++ Simple.Sqlite/SqliteDB.cs
@@ -355,7 +355,7 @@ namespace Simple.Sqlite
         /// <param name="tableName">Name of the table, uses T class name if null</param>
         /// <returns></returns>
         public long Insert<T>(T item, OnConflict resolution = OnConflict.Abort, string tableName = null)
-            => ExecuteScalar<long>(buildInsertSql<T>(resolution, tableName), item);
+            => ExecuteScalar<long>(HelperFunctions.buildInsertSql<T>(typeCollection, resolution, tableName), item);
 
         /// <summary>
         /// Inserts a new T or replace with current T and return it's ID, this method locks the execution.
@@ -423,34 +423,6 @@ namespace Simple.Sqlite
             if (!IsInMemoryDatabase) source.Close();
         }
 
-        private string buildInsertSql<T>(OnConflict resolution, string tableName = null)
-        {
-            var info = typeCollection.GetInfo<T>();
-            if (tableName == null) tableName = info.TypeName;
-
-            var names = getNames(info, !info.IsAnonymousType);
-            var fields = string.Join(",", names);
-            var values = string.Join(",", names.Select(n => $"@{n}"));
-
-            if (resolution == OnConflict.Abort)
-            {
-                return $"INSERT INTO {tableName} ({fields}) VALUES ({values}); SELECT last_insert_rowid();";
-            }
-            else
-            {
-                string txtConflict = resolution switch
-                {
-                    OnConflict.RollBack => "ROLLBACK",
-                    OnConflict.Fail => "FAIL",
-                    OnConflict.Ignore => "IGNORE",
-                    OnConflict.Replace => "REPLACE",
-                    _ => throw new ArgumentException($"Invalid resolution: {resolution}"),
-                };
-
-                return $"INSERT OR {txtConflict} INTO {tableName} ({fields}) VALUES ({values}); SELECT last_insert_rowid();";
-            }
-        }
-
         /* Statics */
         /// <summary>
         /// Creates a InMemory database instance
@@ -464,14 +436,6 @@ namespace Simple.Sqlite
             };
         }
 
-        private static IEnumerable<string> getNames(TypeInfo type, bool needWrite)
-        {
-            return type.Items
-                       .Where(o => !o.Is(DatabaseWrapper.ColumnAttributes.Ignore))
-                       .Where(o => o.CanRead)
-                       .Where(o => !needWrite || o.CanWrite) // Be careful with NOTs and ORs
-                       .Select(o => o.Name);
-        }
 
     }
 }
