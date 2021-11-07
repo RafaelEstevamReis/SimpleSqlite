d0057d1a48a2f0da6d545f484e61cdaefe7e1b15
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-11-07 00:12:40 -0300

    Add BulkInsert

--- Simple.Sqlite/Extension/InsertExtension.cs
+++ Simple.Sqlite/Extension/InsertExtension.cs
@@ -8,5 +8,26 @@ namespace Simple.Sqlite.Extension
         public static long Insert<T>(this ISqliteConnection connection, T item, OnConflict resolution = OnConflict.Abort, string tableName = null)
               => connection.ExecuteScalar<long>(HelperFunctions.buildInsertSql<T>(connection.typeCollection, resolution, tableName), item);
 
+        public static long[] BulkInsert<T>(this ISqliteConnection connection, IEnumerable<T> items, OnConflict resolution = OnConflict.Abort, string tableName = null)
+        {
+            List<long> ids = new List<long>();
+            string sql = HelperFunctions.buildInsertSql<T>(connection.typeCollection, resolution, tableName);
+
+            using var trn = connection.connection.BeginTransaction();
+
+            foreach (var item in items)
+            {
+                using var cmd = new SqliteCommand(sql, connection.connection, trn);
+                HelperFunctions.fillParameters(cmd, item, connection.typeCollection);
+
+                var scalar = cmd.ExecuteScalar();
+                if (scalar is long sL) ids.Add(sL);
+            }
+
+            trn.Commit();
+
+            return ids.ToArray();
+        }
+
     }
 }
