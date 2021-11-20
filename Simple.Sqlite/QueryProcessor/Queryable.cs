9ee93227f8cfe12dcd0f5fb6ff9e696d9da1a119
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-11-20 12:05:24 -0300

    Add queryable classes

---
+++ Simple.Sqlite/QueryProcessor/Queryable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Simple.Sqlite.QueryProcessor
+{
+    public class Queryable<T> : IOrderedQueryable<T>
+    {
+        public Expression Expression { get; private set; }
+        public IQueryProvider Provider { get; private set; }
+        public Type ElementType => typeof(T);
+
+
+        public Queryable(IQueryProvider provider, Expression expression = null)
+        {
+            Provider = provider;
+            Expression = expression ?? Expression.Constant(this);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (Provider.Execute<IEnumerable<T>>(Expression)).GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return (Provider.Execute<System.Collections.IEnumerable>(Expression)).GetEnumerator();
+        }
+
+    }
+}
