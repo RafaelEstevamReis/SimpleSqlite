c55c35e7aaac67b312a43a9df29f3a40b52efb2d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-29 00:17:15 -0300

    Update PrimaryKeyAttribute.cs

--- Simple.Sqlite/Attributes/PrimaryKeyAttribute.cs
+++ Simple.Sqlite/Attributes/PrimaryKeyAttribute.cs
@@ -5,7 +5,7 @@ namespace Simple.Sqlite.Attributes
     /// <summary>
     /// Specify that this column is PrimaryKey
     /// </summary>
-    [Obsolete("Use DatabaseWrapper.Attributes.PrimaryKeyAttribute instead")]
+    [Obsolete("Use DatabaseWrapper.Attributes.PrimaryKeyAttribute instead", true)]
     public class PrimaryKeyAttribute : DatabaseWrapper.Attributes.PrimaryKeyAttribute
     { }
 }
