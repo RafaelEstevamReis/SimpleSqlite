29844e0bc6109f298df26e704d9bda6aba25f868
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-29 00:17:56 -0300

    Update NotNullAttribute.cs

--- Simple.Sqlite/Attributes/NotNullAttribute.cs
+++ Simple.Sqlite/Attributes/NotNullAttribute.cs
@@ -5,7 +5,7 @@ namespace Simple.Sqlite.Attributes
     /// <summary>
     /// Specify that this column should not allow nulls
     /// </summary>
-    [Obsolete("Use DatabaseWrapper.Attributes.NotNullAttribute instead")]
+    [Obsolete("Use DatabaseWrapper.Attributes.NotNullAttribute instead", true)]
     public class NotNullAttribute : DatabaseWrapper.Attributes.NotNullAttribute
     { }
 }
