a162e0481b91af5179c97a37f529a4517db94176
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-08-04 22:10:35 -0300

    Create GuidReadWriteTests.cs

---
+++ UnitTest/SqliteExtensionsTests/GuidTests/GuidReadWriteTests.cs
@@ -0,0 +1,41 @@
+﻿using Simple.Sqlite;
+using Simple.Sqlite.Extension;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace UnitTest.SqliteExtensionsTests.GuidTests
+{
+    public class GuidReadWriteTests
+    {
+        [Fact]
+        public void GuidTests_ReadWrite()
+        {
+            ConnectionFactory.HandleGuidAsByteArray = true;
+
+            using var db = ConnectionFactory.CreateInMemory();
+            db.CreateTables()
+              .Add<MyGuidClass>()
+              .Commit();
+
+            var guid = Guid.NewGuid();
+            // Write GUID, read Bytes
+            db.Insert(new MyGuidClass() { Guid = guid });
+            var c2 = db.Query<MyByteClass>("SELECT * FROM MyGuidClass", null).FirstOrDefault();
+
+            Assert.Equal(guid, new Guid(c2.Guid));
+        }
+
+
+
+        public class MyGuidClass
+        {
+            public Guid Guid { get; set; }
+        }
+        public class MyByteClass
+        {
+            public byte[] Guid { get; set; }
+        }
+
+    }
+}
