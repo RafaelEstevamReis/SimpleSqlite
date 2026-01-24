6697fe0b5eaf33d1c28df9d3520b8fd4488a9640
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2026-01-24 13:10:32 -0300

    Improve CSV ingestion

--- Simple.Sqlite/Extension/CsvIngestionExtensions.cs
+++ Simple.Sqlite/Extension/CsvIngestionExtensions.cs
@@ -3,6 +3,7 @@
 using Simple.DatabaseWrapper;
 using Simple.DatabaseWrapper.Helpers;
 using System;
+using System.IO;
 using System.Text;
 
 /// <summary>
@@ -24,11 +25,52 @@ public static class CsvIngestionExtensions
     /// <param name="quote">CSV quote char</param>
     public static void LoadFromCsvFile<T>(this ISqliteConnection connection, string csvFile, Func<string[], T> mapping, int bufferSize = 10_000, Encoding? encoding = null, char delimiter = ';', char quote = '"')
     {
+        using var fs = File.OpenRead(csvFile);
+        LoadFromCsvStream(connection, fs, encoding ?? Encoding.UTF8, mapping, bufferSize, delimiter, quote);
+    }
+
+#if !NET472
+    /// <summary>
+    /// Load a Zipped CSV file into a database. 
+    /// For custom CSV ingestion try DatabaseWrapper.CsvParser functions and insert with BulkInsert
+    /// </summary>
+    public static void LoadFromCsvZippedFile<T>(this ISqliteConnection connection, string zipFile, Func<string, bool> entryFilter, Func<string[], T> mapping, int bufferSize = 10_000, Encoding? encoding = null, char delimiter = ';', char quote = '"')
+    {
+        using var fs = File.OpenRead(zipFile);
+        using var zip = new System.IO.Compression.ZipArchive(fs, System.IO.Compression.ZipArchiveMode.Read);
+
+        foreach (var entry in zip.Entries)
+        {
+            if (string.IsNullOrEmpty(entry.Name)) continue;
+            if (!entryFilter(entry.FullName)) continue;
+
+            using var steam = entry.Open();
+            LoadFromCsvStream(connection, steam, encoding ?? Encoding.UTF8, mapping, bufferSize, delimiter, quote);
+        }
+    }
+#endif
+
+    /// <summary>
+    /// Load a CSV file stream into a database. 
+    /// For custom CSV ingestion try DatabaseWrapper.CsvParser functions and insert with BulkInsert
+    /// </summary>
+    public static void LoadFromCsvStream<T>(this ISqliteConnection connection, Stream dataStream, Encoding encoding, Func<string[], T> mapping, int bufferSize = 10_000, char delimiter = ';', char quote = '"')
+    {
+        using var sr = new StreamReader(dataStream, encoding);
+        LoadFromCsvStream(connection, sr, mapping, bufferSize, delimiter, quote);
+    }
+
+    /// <summary>
+    /// Load a CSV stream into a database. 
+    /// For custom CSV ingestion try DatabaseWrapper.CsvParser functions and insert with BulkInsert
+    /// </summary>
+    public static void LoadFromCsvStream<T>(this ISqliteConnection connection, StreamReader csvStream, Func<string[], T> mapping, int bufferSize = 10_000, char delimiter = ';', char quote = '"')
+    {
         using DataBuffer<T> buffer = new DataBuffer<T>(bufferSize, data =>
         {
             connection.BulkInsert<T>(data, OnConflict.Ignore);
         });
-        var csvRows = CsvParser.ParseCsvFile(csvFile, encoding, delimiter: delimiter, quote: quote);
+        var csvRows = CsvParser.ParseCsv(csvStream, delimiter: delimiter, quote: quote);
 
         foreach (var row in csvRows)
         {
@@ -36,5 +78,4 @@ public static class CsvIngestionExtensions
         }
         buffer.Flush();
     }
-
 }
