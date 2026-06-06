cc9117a9b8e586d6e29914ff9568fe8a1a313e4d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2026-06-06 12:01:26 -0300

    Update SimpleModel.cs

--- UnitTest/TestModels/SimpleModel.cs
+++ UnitTest/TestModels/SimpleModel.cs
@@ -1,13 +1,12 @@
-﻿using Simple.DatabaseWrapper.Attributes;
+﻿namespace UnitTest.TestModels;
 
-namespace UnitTest.TestModels
+using Simple.DatabaseWrapper.Attributes;
+
+public class SimpleModel
 {
-    public class SimpleModel
-    {
-        [PrimaryKey]
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public float Value { get; set; }
-        public bool Enabled { get; set; }
-    }
+    [PrimaryKey]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Value { get; set; }
+    public bool Enabled { get; set; }
 }
